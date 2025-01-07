d66ac181bde90228649532e1b26bed016b0e8c97
Aakarsh Nair <aakarsh.nair@gmail.com>
2025-01-07 20:42:49 +0100

    adding  dependencies.

---
+++ code/slurm/dependencies.sh
@@ -0,0 +1,13 @@
+pip install gymnasium accelerate transformers databench-eval
+pip install swig
+pip install vllm
+pip install auto-gptq # install auto-gptq package for handling GGUF format.
+pip install auto-gptq --upgrade # Update the auto-gptq library for latest features.
+pip install transformers --upgrade # Update the transformers library.
+pip install optimum # Install the optimum library
+pip install transformers>=4.30.0 # Update transformers to version 4.30.0 or later.
+pip install --upgrade optimum auto-gptq transformers
+pip install openai
+pip install  bitsandbytes
+pip install accelerate
+pip install pandas
