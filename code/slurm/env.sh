f41421701a507f8e6837cebc331956580cbfacd9
Aakarsh Nair <aakarsh.nair@gmail.com>
2025-01-09 12:50:28 +0100

    initial environment.

---
+++ code/slurm/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+module load devel/python/3.10
+source ~/semeval-venv-01-07-2025/bin/activate
+export PYTHONPATH=$PYTHONPATH:/home/tu/tu_tu/tu_zxoxo45/challenges-cl/SemEval25-Task8-Adrianna-Aakarsh-Yang/code
