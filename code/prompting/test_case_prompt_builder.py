0b80eda814c0b141dfcd8e1caf56f30fcf422acb
Aakarsh Nair <aakarsh.nair@gmail.com>
2025-01-11 20:11:23 +0100

    try to refactor prompts aa bit.

---
+++ code/prompting/test_case_prompt_builder.py
@@ -0,0 +1,61 @@
+from jinja2 import Template
+import json
+import pandas as pd
+import os
+
+def generate_dataframe_schma_json(df):
+  schema = {
+       "columns": [
+           {"name": col, "type": str(df[col].dtype)}
+           for col in df.columns
+       ]
+   }
+  json_schema = json.dumps(schema, indent=4)
+  return json_schema
+
+def generate_dataframe_description_json(df):
+  description = df.describe().to_json(orient='index', indent=4)
+  return description
+
+def generate_random_sample_of_n_rows_json(df, n=10):
+    return df.sample(n=n).to_json(orient='records', indent=4)
+
+def generate_dataframe_schma_json(df):
+  schema = {
+       "columns": [
+           {"name": col, "type": str(df[col].dtype)}
+           for col in df.columns
+       ]
+   }
+  json_schema = json.dumps(schema, indent=4)
+  return json_schema
+
+def generate_dataframe_description_json(df):
+  description = df.describe().to_json(orient='index', indent=4)
+  return description
+
+def generate_random_sample_of_n_rows_json(df, n=10):
+    return df.sample(n=n).to_json(orient='records', indent=4)
+
+def build_prompt(row, df, skip_description=["029_NYTimes"]):
+    """
+    Takes a dataset row expects coluums: question, dataset, type or predicted_type
+    """
+    question = row['question']
+    df_random_sample = '{}'
+    if not row['dataset'] in skip_description:
+       df_random_sample = generate_dataframe_description_json(df) 
+
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    template_path = os.path.join(current_dir, "prompt-templates/test_case_prompt_template.jinja")
+    with open(template_path) as file:
+        testcase_template = Template(file.read())
+        
+    prompt = testcase_template.render(
+        schema=generate_dataframe_schma_json(df),
+        description=generate_dataframe_description_json(df),
+        random_sample=df_random_sample,
+        question=question,
+        columns=list(df.columns)
+    )
+    return prompt
