4f2a3cf6d943c245e1459091f1e8e7bd717d36e4
Aakarsh Nair <aakarsh.nair@gmail.com>
2025-01-25 20:50:40 +0100

    wip: still working on parsing rewards.

--- code/evals/parse_rewards.py
+++ code/evals/parse_rewards.py
@@ -5,13 +5,11 @@ import os
 import pandas as pd
 
 from multiprocessing import Pool, Manager, cpu_count
-from utils.dir_utils import (find_all_output_files, 
-                             collect_all_results,
-                             extract_id_from_path)
+from utils.dir_utils import (find_all_output_files, collect_all_results)
 
 from dataloading.semeval_load_dataset import load_phase_dataset
 from runners.push_results_to_finetune import parse_return_statement 
-from py_evaluator.utils import post_process, evaluate_return_statement
+from py_evaluator.utils import evaluate_return_statement
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -142,16 +140,27 @@ def generate_predictions_files(parsed_rewards, output_dir):
         logging.debug(f"Generating predictions for semeval_id: {semeval_id}")
         # filter to keep only eval_results for higest rewards
         records_highest_rewards = [ r for r in parsed_rewards[semeval_id]['parsed_rewards'] if r['reward'] == max([ r['reward'] for r in parsed_rewards[semeval_id]['parsed_rewards'] ]) ] 
+        
         eval_results = [ r['eval_result'] for r in records_highest_rewards ]
         eval_results_lite = [ r['eval_result_lite'] for r in records_highest_rewards ]
-        
-        predictions[semeval_id] = {
-            'eval': max(set(eval_results), key=eval_results.count),
-            'eval_lite': max(set(eval_results_lite), key=eval_results_lite.count)
+
+        selected_records = [ r 
+                            for r in records_highest_rewards 
+                                if r['eval_result'] == max(set(eval_results), key=eval_results.count) 
+                                or r['eval_result_lite'] == max(set(eval_results_lite), key=eval_results_lite.count) ] 
+       
+        selected_eval_value = selected_records[0]['eval_result']
+        selected_eval_lite_value = selected_records[0]['eval_result_lite'] 
+        code_return = selected_records[0]['return']
+
+        predictions[int(semeval_id)] = {
+            'eval': selected_eval_value, #max(set(eval_results), key=eval_results.count),
+            'eval_lite': selected_eval_lite_value, #max(set(eval_results_lite), key=eval_results_lite.count),
+            'code': code_return
         }
     return predictions
 
-def generate_submission_from_predictions(predictions, output_dir, tag="", missing_submission="MISSING_SUBMISSION"):
+def generate_submission_from_predictions(num_questions, predictions, output_dir, tag="", missing_submission="MISSING_SUBMISSION"):
     """
    Submissions are ordered by semeval_id, when we don't have a result we use 
    the token MISSING_SUBMISSION. 
@@ -160,41 +169,53 @@ def generate_submission_from_predictions(predictions, output_dir, tag="", missin
     """
     sorted_prediction_ids = set(sorted(map(int, predictions.keys())))
     max_id = max(sorted_prediction_ids)
-    logging.debug(f"Generating predictions files for {max_id} ids.")
+    logging.debug(f"Generating predictions files for max_id{max_id}, number of questions: {num_questions} ids.")
+
     def single_line_output(prog_output):
         """
-        force program output to be single line.
+        Force program output to be single line.
         """
-        prog_output = prog_output.replace("\n", " ") 
+        # Replace escaped newline characters with actual newlines
+        prog_output = prog_output.replace("\\r\\n", "\r\n").replace("\\r", "\r").replace("\\n", "\n")
+    
+        # Replace different types of newline characters with a space
+        prog_output = prog_output.replace("\r\n", " ").replace("\r", " ").replace("\n", " ")
+        # Optionally strip leading and trailing whitespace
+        prog_output = prog_output.strip()
         return prog_output
-        
+ 
     with open(f"{output_dir}/predictions{tag}.txt", "w+") as f:
-        for idx in range(0, max_id):
+        for idx in range(0, num_questions): 
             if idx in sorted_prediction_ids:
-                f.write(f"{single_line_output(predictions[str(idx)]['eval'])}\n")
+                f.write(f"{single_line_output(predictions[idx]['eval'])}\n")
             else:
                 f.write(f"{missing_submission}\n")
 
     with open(f"{output_dir}/predictions_lite{tag}.txt", "w+") as f:
-        for idx in range(0, max_id):
+        for idx in range(0, num_questions): # 
             if idx in sorted_prediction_ids:
-                f.write(f"{single_line_output(predictions[str(idx)]['eval_lite'])}\n")
+                f.write(f"{single_line_output(predictions[idx]['eval_lite'])}\n")
             else:
                 f.write(f"{missing_submission}\n")
-    logging.debug(f"Generated predictions files at: {output_dir}")
+
+    logging.debug(f"Generated predictions files at : {output_dir}")
            
 if __name__ == "__main__":
     root_dir = os.path.expanduser("~/.cache/pipeline-runs")
+
     parser = argparse.ArgumentParser(description="Parse rewards from the output of the reward function.")
     parser.add_argument("--root-dir", type=str, default=root_dir, help="The path to the directory containing the parquet files.")
     parser.add_argument("--base-model", type=str, default="codellama/CodeLlama-7b-Python-hf", help="The name of the repository.")
     parser.add_argument("--phase", type=str, default="competition", help="The phase of the competition.")
     parser.add_argument("--split", type=str, default="dev", help="The dataset split.")
+
     # parser.add_argument
     args = parser.parse_args()
+    questions_dataset, datasets_map = load_phase_dataset(phase=args.phase, split=args.split)
+    num_questions = len(questions_dataset)
+    logging.debug(f"Number of questions: {len(questions_dataset)}")
 
     if not os.path.exists("parsed_rewards.json"):
-        questions_dataset, datasets_map = load_phase_dataset(phase=args.phase, split=args.split)
         _, lite_datasets_map = load_phase_dataset(phase=args.phase, split=args.split, lite=True)
         reward_data = read_reward_files(args.root_dir, args.base_model)
 
@@ -209,13 +230,19 @@ if __name__ == "__main__":
         with open("parsed_rewards.json", "w") as f:
             f.write(json.dumps(parsed_rewards, indent=4))
 
-    # reload the parsed rewards
+    
+    # TODO: Correlation statistics between reward and answers. 
+    # We can use this to determine if the reward function is working as expected.
+    
+    # TODO: Semantic Equivalence scores between codes on random subsets of data, we can use this 
+    # to determine degree of uncertainty reload the parsed rewards.
+    
     with open("parsed_rewards.json", "r") as f:
         parsed_rewards = json.load(f)
     predictions = generate_predictions_files(parsed_rewards, output_dir="predictions.json")
     
     # Evaluate the return statements
     with open("predictions.json", "w") as f:
-        f.write(json.dumps(predictions, indent=4))
+        f.write(json.dumps(predictions, sort_keys=True, indent=4))
 
-    generate_submission_from_predictions(predictions, output_dir="../submissions", tag="_latest")
+    generate_submission_from_predictions(num_questions, predictions, output_dir="../submissions", tag="_latest")
